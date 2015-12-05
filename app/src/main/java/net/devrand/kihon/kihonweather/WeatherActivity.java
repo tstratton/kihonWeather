bc513a5ab90d31badba7d093bf1d091159264ef8
tstratton <tstratton@users.noreply.github.com>
2015-12-04 22:56:45 -0800

    use okhttp and asynctask to retrieve Wunderground api weather data

--- app/src/main/java/net/devrand/kihon/kihonweather/WeatherActivity.java
+++ app/src/main/java/net/devrand/kihon/kihonweather/WeatherActivity.java
@@ -1,15 +1,32 @@
 package net.devrand.kihon.kihonweather;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    private static final String TAG = "WeatherActivity";
+    private static final String BASE_URL = "http://api.wunderground.com/api/%s/conditions/forecast/q/%s/%s.json";
+
+    private static final String API_KEY = "XXXX_WUNDERGROUND_API_KEY_XXXX";
+    private static final String STATE_STRING = "CA";
+    private static final String CITY_STRING = "San_Francisco";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +35,13 @@ public class WeatherActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        textView = (TextView) findViewById(R.id.text);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                new GetDataTask().execute(BASE_URL, API_KEY, STATE_STRING, CITY_STRING);
             }
         });
     }
@@ -48,5 +66,42 @@ public class WeatherActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class GetDataTask extends AsyncTask<String, Integer, String> {
+        protected String doInBackground(String... urls) {
+            OkHttpClient client = new OkHttpClient();
+            Response response;
+
+            String url = String.format(urls[0], urls[1], urls[2], urls[3]);
+
+            Log.d(TAG, "Getting " + url);
+
+            Request request = new Request.Builder()
+                    .url(url)
+                    .build();
+
+            try {
+                response = client.newCall(request).execute();
+                String json = response.body().string();
+                return String.format("%d chars\n%s\nAPI key '%s'\nResult for %s, %s\n%s",
+                        json.length(), urls[0], urls[1], urls[2], urls[3], json);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                return null;
+            }
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+
+        protected void onPreExecute() {
+            textView.setText("Getting Data..");
+        }
+
+        protected void onPostExecute(String result) {
+            textView.setText(result);
+        }
     }
 }
